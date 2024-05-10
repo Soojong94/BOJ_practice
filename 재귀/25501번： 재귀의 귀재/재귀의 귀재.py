3181519138ba1e9829a477719661116c9cea98ce
Soojong Kim <sujjong456@gmail.com>
2024-05-10 14:03:06 +0900

    0510

--- 재귀/25501번： 재귀의 귀재/재귀의 귀재.py
+++ 재귀/25501번： 재귀의 귀재/재귀의 귀재.py
@@ -1,30 +1,11 @@
 #  **************************************************************************  #
 #                                                                              #
 #                                                       :::    :::    :::      #
-#    Problem Number: 10870                             :+:    :+:      :+:     #
+#    Problem Number: 25501                             :+:    :+:      :+:     #
 #                                                     +:+    +:+        +:+    #
 #    By: sujjong456 <boj.kr/u/sujjong456>            +#+    +#+          +#+   #
 #                                                   +#+      +#+        +#+    #
-#    https://boj.kr/10870                          #+#        #+#      #+#     #
-#    Solved: 2024/05/09 11:14:36 by sujjong456    ###          ###   ##.kr     #
+#    https://boj.kr/25501                          #+#        #+#      #+#     #
+#    Solved: 2024/05/10 09:34:05 by sujjong456    ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
-
-N = int(input())
-num0 = 0
-num1 = 1
-answer_num = 1
-
-if N == 0:
-    print(0)
-elif N == 1:
-    print(1)
-
-elif N >= 2:
-
-    for i in range(1, N):
-        answer_num = num0 + num1
-        num0 = num1
-        num1 = answer_num
-
-    print(answer_num)
