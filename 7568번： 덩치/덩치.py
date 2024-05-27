bf2fddfa32753992eef93531b490cedefe4061b4
Soojong Kim <soojong456@gmail.com>
2024-05-27 21:08:41 +0900

    0527

--- 7568번： 덩치/덩치.py
+++ 7568번： 덩치/덩치.py
@@ -10,14 +10,29 @@
 #                                                                              #
 #  **************************************************************************  #
 
+# 
+import sys
+
+def input() :
+  return sys.stdin.readline()
+
 N = int(input())
 people = []
-cnt = 0
 
-
+for i in range(1 , N+1) :
+  p, q = map(int, input().split())
+  people.append([i, p, q, 1])
 
 for i in range(N) :
-  p, q = map(int, input().split())
-  people.append([p,q])
-  
-people_sorted = sorted(people, key=lambda x : (x[0], x[1]))
+  rank = 1
+  for j in range(N):
+    if i != j and people[i][1] < people[j][1] and people[i][2] < people[j][2] :
+      rank += 1
+  people[i][3] = rank
+
+people.sort(key=lambda x : x[0])
+
+for i in range(N) :
+  print(people[i][3], end = ' ')
+
+
