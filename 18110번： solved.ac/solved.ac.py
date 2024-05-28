2aea2c34f2c6b472e1b46b278abaa25e9a760f9d
Soojong Kim <soojong456@gmail.com>
2024-05-28 21:52:57 +0900

    0528

--- 18110번： solved.ac/solved.ac.py
+++ 18110번： solved.ac/solved.ac.py
@@ -9,3 +9,28 @@
 #    Solved: 2024/05/28 09:05:40 by sujjong456    ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
+n = int(input())
+
+score = []
+
+for i in range(n):
+    score.append(int(input()))
+
+score.sort()
+
+
+def roundUp(num):
+    if (num - int(num)) >= 0.5:
+        return int(num) + 1
+    else:
+        return int(num)
+
+
+cut = roundUp(n * 0.15)
+sum_num = sum(score[cut : n - cut])
+
+if sum_num == 0:
+    print(0)
+else:
+
+    print(roundUp(sum_num / (n - (2 * cut))))
--- 7568번： 덩치/덩치.py
+++ 7568번： 덩치/덩치.py
@@ -10,29 +10,29 @@
 #                                                                              #
 #  **************************************************************************  #
 
-# 
+#
 import sys
 
-def input() :
-  return sys.stdin.readline()
+
+def input():
+    return sys.stdin.readline()
+
 
 N = int(input())
 people = []
 
-for i in range(1 , N+1) :
-  p, q = map(int, input().split())
-  people.append([i, p, q, 1])
+for i in range(1, N + 1):
+    p, q = map(int, input().split())
+    people.append([i, p, q, 1])
 
-for i in range(N) :
-  rank = 1
-  for j in range(N):
-    if i != j and people[i][1] < people[j][1] and people[i][2] < people[j][2] :
-      rank += 1
-  people[i][3] = rank
+for i in range(N):
+    rank = 1
+    for j in range(N):
+        if i != j and people[i][1] < people[j][1] and people[i][2] < people[j][2]:
+            rank += 1
+    people[i][3] = rank
 
-people.sort(key=lambda x : x[0])
+people.sort(key=lambda x: x[0])
 
-for i in range(N) :
-  print(people[i][3], end = ' ')
-
-
+for i in range(N):
+    print(people[i][3], end=" ")
