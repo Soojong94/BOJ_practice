fcb6341f18246599924cf082ffd73ae40ad3f780
Soojong Kim <soojong456@gmail.com>
2024-06-07 19:35:25 +0900

    0607

--- 1463번： 1로 만들기/1로 만들기.py
+++ 1463번： 1로 만들기/1로 만들기.py
@@ -10,17 +10,25 @@
 #                                                                              #
 #  **************************************************************************  #
 
+import sys
+
+input = sys.stdin.readline
+
 
 def makeOne(N):
-    if N == 1:
-        return 0
-    cnt = 1
-    if N % 3 == 0:
-        return cnt + makeOne(N // 3)
-    elif N % 2 == 0:
-        return cnt + makeOne(N // 2)
-    else:
-        return cnt + makeOne(N - 1)
+    dp = [0] * (N + 1)
+
+    for i in range(2, N + 1):
+        dp[i] = dp[i - 1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    return dp[N]
 
 
-print(makeOne(int(input())))
+N = int(input().strip())
+print(makeOne(N))
